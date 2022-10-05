b79a2ae18d4e3ccbd1bdf8cee62870cba3d03c2e
mehmetdemircioglu <112698694+mehmetdemircioglu@users.noreply.github.com>
2022-10-05 11:23:33 +0300

    upload file edit

--- src/firebaseStorage/FileIO.js
+++ src/firebaseStorage/FileIO.js
@@ -1,25 +1,45 @@
 import React from "react";
 import { app } from "../firebaseConfig";
-import { getStorage, ref, uploadBytesResumable } from "firebase/storage";
-import { FileUpload } from 'primereact/fileupload';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { FileUpload } from "primereact/fileupload";
 
 const FileIO = () => {
+  const storage = getStorage(app,"gs://mkk-test-4bbac.appspot.com");
 
-  const storage = getStorage(app);
-
-  const onBasicUpload =(event)=> {
-
-    debugger
+  const onBasicUpload = (event) => {
+    debugger;
     console.log(event.files);
 
     const file = event.files[0];
 
-    if(!file) return;
+    if (!file) return;
 
-    const storageRef = ref(storage,'files/'+file.name);
-    const uploadTask = uploadBytesResumable(storageRef,file);
+    const storageRef = ref(storage,  file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file);
 
-  }
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        console.log(progress);
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          console.log(url);
+        });
+      }
+    );
+  };
 
   return (
     <div>
@@ -30,7 +50,7 @@ const FileIO = () => {
         customUpload={true}
         maxFileSize={1000000000}
         auto={true}
-        uploadHandler={(e)=>onBasicUpload(e)}
+        uploadHandler={(e) => onBasicUpload(e)}
       />
     </div>
   );
