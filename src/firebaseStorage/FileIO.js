ea721cbbbc2e36c53f80e6f65b39462d46a9aa4b
mehmetdemircioglu <112698694+mehmetdemircioglu@users.noreply.github.com>
2022-10-05 10:41:54 +0300

    file upload download

---
+++ src/firebaseStorage/FileIO.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import { app } from "../firebaseConfig";
+import {getStorage} from 'firebase/storage';
+
+const FileIO =()=> {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FileIO;
